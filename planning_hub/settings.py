8a0082708efe2c0f5c3cb4c2a30d1289678e2fa7
Liga Auguste <ligaauguste@gmail.com>
2026-08-06 20:59:52 +0300

    Fix CSRF failure on demo server behind HTTPS proxy
    Add CSRF_TRUSTED_ORIGINS from environment variable so Django accepts
    POST requests when running behind an nginx SSL termination proxy.

--- planning_hub/settings.py
+++ planning_hub/settings.py
@@ -29,6 +29,8 @@ DEMO_MODE = os.environ.get('DEMO_MODE', 'false').lower() == 'true'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
 
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split()
+
 
 # Application definition
 
